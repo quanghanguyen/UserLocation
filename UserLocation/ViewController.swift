742a95fedf020117b6cb96380c5c9344d7bae609
QuangHa <duongthien.mcs@gmail.com>
2022-10-03 10:37:11 +0700

    commit 1

--- UserLocation/ViewController.swift
+++ UserLocation/ViewController.swift
@@ -6,14 +6,46 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
+    
+    var location: CLLocationManager?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        addView()
+        getLocation()
+    }
+    
+    private func getLocation() {
+        location = CLLocationManager()
+        location?.delegate = self
+//        location?.allowsBackgroundLocationUpdates = true
+        location?.requestAlwaysAuthorization()
+        location?.startUpdatingLocation()
+    }
+    
+    private func addView() {
+        locationInfo.frame = CGRect(x: 20, y: view.bounds.height/2 - 100, width: view.bounds.width - 40, height: 120)
+        view.addSubview(locationInfo)
     }
 
+    private let locationInfo: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .white
+        label.textColor = .black
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        return label
+    }()
+}
 
+extension ViewController : CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            locationInfo.text = "Lat: \(location.coordinate.latitude) \n Long: \(location.coordinate.longitude)"
+        }
+    }
 }
 
