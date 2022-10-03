1d26a63de31715e8fc90c2b0fb77766840122e41
QuangHa <duongthien.mcs@gmail.com>
2022-10-03 09:24:59 +0700

    Initial Commit

---
+++ UserLocationUITests/UserLocationUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  UserLocationUITestsLaunchTests.swift
+//  UserLocationUITests
+//
+//  Created by Nguyen Quang Ha on 03/10/2022.
+//
+
+import XCTest
+
+final class UserLocationUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
